4d2e0111f8932a2c3476312ae96bfd8b277bd7c6
Arne Brasseur <arne.brasseur@rendaosolutions.com>
2012-08-13 12:59:18 +0200

    Do not do a connect to database in the postgres dumps, makes it easier to restore a dump under a different name

--- files/default/make_backups.sh
+++ files/default/make_backups.sh
@@ -2,5 +2,5 @@
 
 for db in  `echo '\l' | psql | grep openerp | cut -d ' ' -f 2`
 do
-   pg_dump -C $db | bzip2 > $HOME/backups/${db}_`date "+%Y%m%d-%H%M"`.sql.bz
+   pg_dump $db | bzip2 > $HOME/backups/${db}_`date "+%Y%m%d-%H%M"`.sql.bz
 done
